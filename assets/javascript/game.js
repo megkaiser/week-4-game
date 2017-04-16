1f4cfdbdf206b3ece8cf0e285d21384a6f7a24a0
Meghan Kaiser <mkaiser@neuroskills.com>
2017-04-15 23:23:25 -0700

    Final version

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,31 +6,69 @@ var crystal1 = Math.floor(Math.random() * 12) + 1;
 var crystal2 = Math.floor(Math.random() * 12) + 1;
 var crystal3 = Math.floor(Math.random() * 12) + 1;
 var crystal4 = Math.floor(Math.random() * 12) + 1;
+var total = 0;
+var wins = 0;
+var losses = 0;
 
-var computerGuess = Math.floor(Math.random() * 102) + 19;
+var computerGuess = Math.floor(Math.random() * 101) + 19;
+var winLossCheck = function() {
+			if (total === computerGuess) {
+				wins++;
+				$("#wins").html("Wins: " + wins);
+				$("#gameResult").html("You Won!");
+				reset();
+		} else if (total > computerGuess) {
+			losses++;
+			$("#losses").html("Losses: " + losses);
+			$("#gameResult").html("You Lost");
+			reset();
+		}
+}
 
+function reset () {
+			total = 0;
+			computerGuess = Math.floor(Math.random() * 101) + 19;
+			crystal1 = Math.floor(Math.random() * 12) + 1;
+			crystal2 = Math.floor(Math.random() * 12) + 1;
+			crystal3 = Math.floor(Math.random() * 12) + 1;
+			crystal4 = Math.floor(Math.random() * 12) + 1;
+			$(".num").html(computerGuess);
+}
 
 	$("#crystal1").on("click", function(){
-		$("#yourscore").html(crystal1);
+		total += crystal1;
+		$("#yourscore").html(total);
 		console.log(crystal1);	
+		winLossCheck();
 	});	
 
 	$("#crystal2").on("click", function(){
-		$("#yourscore").html(crystal2);
-		console.log(crystal2);	
+		total += crystal2
+		$("#yourscore").html(total);
+		console.log(crystal2);
+		winLossCheck();	
 	});
 
 	$("#crystal3").on("click", function(){
-		$("#yourscore").html(crystal3);
-		console.log(crystal3);	
+		total += crystal3
+		$("#yourscore").html(total);
+		console.log(crystal3);
+		winLossCheck();	
 	});
 
 	$("#crystal4").on("click", function(){
-		$("#yourscore").html(crystal4);
-		console.log(crystal4);	
+		total += crystal4
+		$("#yourscore").html(total);
+		console.log(crystal4);
+		winLossCheck();	
 	});
 
 	$(".num").html(computerGuess);
 
 });
 
+
+
+// prevent crystals from generating the same number
+
+// reset game
